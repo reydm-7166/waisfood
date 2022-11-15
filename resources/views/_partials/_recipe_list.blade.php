3520c3e648cc344bb05bdfd8fda5f71198bc6d9c
reydm-7166 <reymond.domingo.716@gmail.com>
2022-11-15 20:55:35 +0800

    added dishes table and others

--- resources/views/_partials/_recipe_list.blade.php
+++ resources/views/_partials/_recipe_list.blade.php
@@ -1,27 +1,3 @@
-<div id="recipe_item" class="border border-primary me-4">
-
-</div>
-
-<div id="recipe_item" class="border border-primary me-4">
-
-</div>
-
-<div id="recipe_item" class="border border-primary me-4">
-
-</div>
-
-<div id="recipe_item" class="border border-primary me-4">
-
-</div>
-
-<div id="recipe_item" class="border border-primary me-4">
-
-</div>
-
-<div id="recipe_item" class="border border-primary me-4">
-
-</div>
-
-<div id="recipe_item" class="border border-primary me-4">
+<div id="recipe_item" class="border border-primary me-4 mt-2">
 
 </div>
