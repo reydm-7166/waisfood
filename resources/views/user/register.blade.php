f3334281b237dd78d5f86609ed7545219cabd233
Keemchard <keemchardtamio@gmail.com>
2022-11-29 17:32:26 +0800

    add new register layout

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -3,6 +3,7 @@
 @section('less_import')
     <link rel="stylesheet/less" type="text/css" href="{{ asset('css/register.less') }}" />
     <script src="https://cdn.jsdelivr.net/npm/less" ></script>
+    @vite('resources/css/app.css')
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
         @import url('https://fonts.googleapis.com/css2?family= Lexend+Deca: wght@300;400 &display=swap');
@@ -22,7 +23,28 @@
 @section('body')
 
 {{-- new register layout --}}
-
+<div class="w-[100%] h-[100vh] flex justify-center items-center">
+    <form class="p-[20px] flex flex-col justify-center items-center">
+        <div class="s-logo">
+            <img class="w-[200px] ml-[-15px]" src="\assets\Yellow and Green Banana Fruit Food Logo (1).png" alt="Wais logo">
+        </div>
+        <div class="s-title font-bold text-[38px] mt-[-15px]">Create new Account</div>
+        <div class="s-sub mt-[18px] mb-[15px]">Already Registered? <a href="/">Login</a></div>
+        <div class="s-input-field flex flex-col m-[10px] w-[70%]">
+            <label>FIRST NAME</label>
+            <input class="rounded" type="text">
+            <label>LAST NAME</label>
+            <input class="rounded" type="text">
+            <label>EMAIL</label>
+            <input class="rounded" type="email">
+            <label>PASSWORD</label>
+            <input class="rounded" type="password">
+            <label>AGE</label>
+            <input class="rounded" type="number">
+        </div>
+        <button type="submit" class="signup-btn text-[white] font-bold bg-[black] p-[10px] w-[170px] text-center mt-[10px]">Sign Up</button>
+    </form>
+</div>
 {{-- new register layout --}}
 
 <body class="bg-secondary w-100">
