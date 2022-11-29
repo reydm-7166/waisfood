82225581687f7800368498e5bac552998d2022c1
Keemchard <keemchardtamio@gmail.com>
2022-11-29 17:38:55 +0800

    add reusable navbar and food cards

---
+++ resources/views/livewire/reusable/food-cards.blade.php
@@ -0,0 +1,5 @@
+<div class="fav-food w-[auto] flex flex-col justify-center items-center">
+    <img class="w-[auto] mb-[15px]" src="\assets\login-image.png" alt="food">
+    <p class="time text-[15px] text-[gray] ">60 Minutes</p>
+    <div class="p-[20px] text-center"><p class="food-title font-bold text-[15px]">Cheesy Broccoli-stuffed Chicken Breasts</p></div>
+</div>
---
+++ resources/views/livewire/reusable/navbar.blade.php
@@ -0,0 +1,13 @@
+<div class="nav w-[100%] pl-[35px] pr-[35px] pt-[10px] pb-[10px] flex items-center text-[#f6941c]">
+    <div class="nav-logo bg-[transparent] flex-1">
+         <img class="w-[120px] ml-[-20px]" src="\assets\Yellow and Green Banana Fruit Food Logo (1).png" alt="Wais logo"></div>
+    <div class="nav-menu bg-[transparent] flex-1">
+     <ul class="flex justify-around items-center">
+         <li class="text-[20px]">Home</li>
+         <li class="text-[20px]">Generator</li>
+         <li class="text-[20px]">About Us</li>
+         <li class="text-[20px]">Blog</li>
+         <li class="nav-acc text-[25px] flex items-center justify-center"><i class="fa-regular fa-user"></i></li>
+     </ul>
+    </div>
+ </div>
