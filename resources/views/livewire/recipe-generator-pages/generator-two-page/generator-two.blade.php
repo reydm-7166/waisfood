be4360f6e9c863db94be5c47d555f5c5f20bbbdd
Keemchard <keemchardtamio@gmail.com>
2022-11-29 18:48:26 +0800

    add generator 2 layout

--- resources/views/livewire/recipe-generator-pages/generator-two-page/generator-two.blade.php
+++ resources/views/livewire/recipe-generator-pages/generator-two-page/generator-two.blade.php
@@ -1,3 +1,106 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class="generator-two">
+    <div class="recipe-two-nav">@livewire('reusable.navbar')</div>
+    <div class="gen-two-con w-[66%] m-[auto] mt-[20px]">
+        <div><p class="text-[30px] text-center mb-[50px] text-[#f6941c]">RECIPE GENERATOR</p></div>
+        <!-- GENERATED -->
+        <div class="generated flex">
+            <div class="gen-con flex-1 mr-[5px] h-[auto] relative">
+                <img class="w-[100%]" src="\assets\login-image.png" alt="food">
+                <div class="mt-[30px] max-h-[600px] overflow-y-scroll">
+                    <div class="flex mb-[13px]">
+                        <div class="admin mr-[10px] text-[12px]"><i class="fa-regular fa-user text-[#6FB43F] mr-[5px]"></i> Admin</div>
+                        <div class="comments text-[12px]"><i class="fa-regular fa-comment text-[#6FB43F] mr-[5px]"></i> 2 Comments</div>
+                    </div>
+                    <div> <p class="text-[36px] mb-[20px]">Pan Pizza</p> </div>
+                    <div class="text-[14px]"> 
+                        <p class="mb-[20px]">INGREDIENTS: Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> 
+                        <div class="leading-[25px]">
+                            <p>Directions</p>
+                            <p>1) Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum sequi unde, corporis ipsam molestias in excepturi? Perferendis beatae dolor veniam alias, harum unde modi eum id, rem illum dolore. Tenetur!</p>
+                            <p>2) Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus esse earum, quibusdam rem consequuntur qui? At quasi eveniet ab consectetur corporis quia laboriosam minima doloribus, velit ratione consequuntur corrupti impedit ullam quae inventore voluptas hic, quos nam, rem voluptates voluptatum? Tempore, inventore, quibusdam tenetur pariatur similique perspiciatis odit debitis incidunt ducimus quam aliquid neque nisi eius provident harum alias esse.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- absolute pagination -->
+                <div class="pagination flex justify-between items-center absolute w-[100%] bottom-0 mt-[100px]">
+                    <div class="ml-[10px] tag">Tags: Recipe, Food, Healthy</div>
+                    <div class="flex mr-[40px]">
+                        <div class="p-c w-[35px] h-[35px] flex items-center justify-center rounded-[50%] mr-[20px] bg-[#6FB43F]">1</div>
+                        <div class="p-c w-[35px] h-[35px] flex items-center justify-center rounded-[50%] mr-[20px] bg-[#6FB43F]">2</div>
+                        <div class="p-c w-[35px] h-[35px] flex items-center justify-center rounded-[50%] mr-[20px] bg-[#6FB43F]">3</div>
+                        <div class="p-c w-[35px] h-[35px] flex items-center justify-center rounded-[50%] mr-[20px] bg-[#6FB43F]">4</div>
+                    </div>
+    
+                </div>
+            </div>
+            <div class="com-ad w-[30%] ">
+                <img src="\assets\Community Ad.png" alt="ads">
+            </div>
+        </div>
+        <!-- COM SEC -->
+        <div class="mt-[60px] com-sec">
+            <div>
+                <p class="text-[30px] mb-[20px]">2 Comments</p>
+                <!-- actual comments -->
+                <div class="flex mb-[30px]">
+                    <div class="mr-[30px]">
+                        <div class="com-prof w-[120px] h-[120px] bg-[#2e2f31] rounded-[50%]"></div>
+                    </div>
+                    <div>
+                        <div class="text-[14px]">
+                            <p class="name font-bold mb-[20px]">Kevin Martin</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci porro obcaecati animi ipsa natus vel voluptates labore at! Recusandae, in!</p>
+                        </div>
+                        <div class="mt-[20px] text-right">
+                            <button> 
+                                <img class="w-[20px]" src="\assets\upvote and downvote button.png" alt="upvote and downvote button">
+                            </button>
+                            <button>
+                                 <img class="reverse w-[20px]" src="\assets\upvote and downvote button.png" alt="upvote and downvote button">
+                            </button>
+                           
+                        </div>
+                    </div>
+                </div>
+                <div class="flex">
+                    <div class="mr-[30px] mb-[30px]">
+                        <div class="com-prof w-[120px] h-[120px] bg-[#2e2f31] rounded-[50%]"></div>
+                    </div>
+                    <div>
+                        <div class="text-[12px]">
+                            <p class="name font-bold mb-[20px]">Kevin Martin</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, obcaecati. Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci porro obcaecati animi ipsa natus vel voluptates labore at! Recusandae, in!</p>
+                        </div>
+                        <div class="mt-[20px] text-right">
+                            <button> 
+                                <img class="w-[20px]" src="\assets\upvote and downvote button.png" alt="upvote and downvote button">
+                            </button>
+                            <button>
+                                 <img class="reverse w-[20px]" src="\assets\upvote and downvote button.png" alt="upvote and downvote button">
+                            </button>
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- comment-form -->
+            <div class="com-form mt-[30px] flex justify-center mb-[40px] w-[65%]  m-[auto]">
+                <div class="w-[100%] text-center">
+                    <p class="text-[30px] text-left">Leave a Comment</p>
+                    <form class="mt-[30px]">
+                        <div class="mb-[23px] flex justify-between">
+                            <input class="bg-[#F7F6F3] p-[12px] w-[48%]" type="text" placeholder="Your Name">
+                            <input class="bg-[#F7F6F3] p-[12px] w-[48%]" type="email" placeholder="Email Address">
+                        </div>
+                        <div>
+                            <textarea class="bg-[#F7F6F3] p-[12px] w-[100%]" name="" id="" cols="48" rows="8" placeholder="Write Message"></textarea>
+                        </div>
+                        <div class="mt-[20px] text-left">
+                            <input class="bg-[#f6941c] text-[white] rounded p-[12px]" type="submit" value="Submit Comment">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
