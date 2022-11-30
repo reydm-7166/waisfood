6841f13b8c15151142bbc8108611670b288d0928
Keemchard <keemchardtamio@gmail.com>
2022-11-30 19:09:35 +0800

    add success modal layout

---
+++ app/Http/Livewire/Reusable/Success.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Reusable;
+
+use Livewire\Component;
+
+class Success extends Component
+{
+    public function render()
+    {
+        return view('livewire.reusable.success');
+    }
+}
---
+++ resources/views/livewire/reusable/success.blade.php
@@ -0,0 +1,15 @@
+ <div class="terms mb-[20px] flex items-center text-[#f6941c] h-[150px] p-[20px] pl-[40px] pr-[40px] bg-[#FAF4D4] relative w-[500px] m-[auto]">
+    <div class="mr-[30px]">
+        <button class=" font-bold border-[3px] border-solid border-[#f6941c] flex justify-center items-center w-[40px] h-[40px] rounded-[50%]">
+                ✔
+        </button>
+    </div>
+    <div>
+       <p class="font-bold">Success</p>
+       <p class="text-[15px]">No more waste! 2 recipes found.</p>
+    </div>
+    <div class="absolute top-6 right-6">
+        ❌
+    </div>
+</div>
+ 
