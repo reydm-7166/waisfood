81b1074fc0600c70e69b1a9ed902a0e4d5d7bb43
Keemchard <keemchardtamio@gmail.com>
2022-11-30 22:17:22 +0800

    add dropdown layout on user at nav

--- resources/views/livewire/reusable/navbar.blade.php
+++ resources/views/livewire/reusable/navbar.blade.php
@@ -7,7 +7,32 @@
         <li class="text-[20px]">Generator</li>
         <li class="text-[20px]">About Us</li>
         <li class="text-[20px]">Blog</li>
-        <li class="nav-acc text-[25px] flex items-center justify-center"><i class="fa-regular fa-user"></i></li>
+        <li class="nav-acc text-[25px] flex items-center justify-center">
+            <div class="text-[#f6941c]">
+                <i class="fa-regular fa-user"></i>
+            </div>
+            <div class="nav-content">
+                <ul class="text-[gray]">
+                    <div class="p-[10px] pl-0 pr-0 bg-[white]"></div>
+                    <div class="tri"></div>
+                    <li class="text-[15px] p-[12px] mt-[14px] flex justify-between w-[100%]">
+                        <a href="#">VIEW MY PROFILE</a> <i class="fa-regular fa-user"></i>
+                    </li>
+                    <li class="text-[15px] p-[12px] flex justify-between w-[100%]">
+                        <a href="#">VIEW SAVED</a> <i class="fa-regular fa-user"></i>
+                    </li>
+                    <li class="text-[15px] p-[12px] flex justify-between w-[100%]">
+                        <a href="#">CREATE POST</a> <i class="fa-regular fa-user"></i>
+                    </li>
+                    <li class="text-[15px] p-[12px] flex justify-between w-[100%]">
+                        <a href="#">READ MORE</a> <i class="fa-regular fa-user"></i>
+                    </li>
+                    <li class="text-[15px] p-[12px] flex justify-between w-[100%]">
+                        <a href="#">SIGN OUT</a> <i class="fa-regular fa-user"></i>
+                    </li>
+                </ul>
+            </div>
+        </li>
     </ul>
    </div>
 </div>
