8f643132efb28d0b2abb881dde1ef4703ee9c955
Keemchard <keemchardtamio@gmail.com>
2022-12-05 16:06:43 +0800

    add drop down

--- resources/views/livewire/pages/read-more-page/read-more.blade.php
+++ resources/views/livewire/pages/read-more-page/read-more.blade.php
@@ -7,6 +7,14 @@
             @livewire("reusable.profile-card")
         </div>
         <div class="w-[70%]  pl-[50px] ">
+            <div class="w-[100%] flex justify-end mb-[30px]">
+                <select name="cars" id="cars" class="drp bg-[#F7F6F3] rounded w-[200px]  p-[15px] pl-[20px] pr-[30px] bg-[transparent]">
+                    <option value="Browse">Filter by</option>
+                    <option value="Hello">View my post only</option>
+                    <option value="World">View my recipes only</option>
+                    <option value="KC">View all</option>
+                </select>
+            </div>
              @livewire("pages.read-more-page.components.read-more-cards")
              @livewire("pages.read-more-page.components.read-more-cards")
              @livewire("pages.read-more-page.components.read-more-cards")
