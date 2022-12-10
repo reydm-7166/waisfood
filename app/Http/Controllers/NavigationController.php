815023b4545adb4d0e105b756238ff9b71c528fa
Virus5600 <satchi5600@gmail.com>
2022-12-10 14:07:13 +0800

    Update 20221210 - Admin Frontend

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -6,8 +6,28 @@ use Illuminate\Http\Request;
 
 class NavigationController extends Controller
 {
-    public function index()
-    {
-        return view('user.index');
-    }
+	public function index() {
+		return view('user.index');
+	}
+
+	// NEWLY ADDED ROUTES
+	public function registration() {
+		return view('admin.registration');
+	}
+
+	public function dashboard() {
+		return view('admin.dashboard');
+	}
+
+	public function userManagement() {
+		return view('admin.user-management');
+	}
+
+	public function postRecipeProposal() {
+		return view('admin.post-recipe-proposal');
+	}
+
+	public function contentManagement() {
+		return view('admin.content-management');
+	}
 }
---
+++ resources/views/admin/user-management.blade.php
@@ -0,0 +1,70 @@
+@extends('layout.admin')
+
+@section('title', 'User Management')
+
+@section('body')
+<div class="overflow-x-auto pt-5">
+	<table class="table my-2 border-0 w-100 position-relative mt-5">
+		<thead>
+			<tr class="card-body my-2 py-2 shadow-sm fw-bold">
+				<th class="text-orange border-0" colspan="8">Users</th>
+			</tr>
+
+			<tr class="card-body my-2 py-2 shadow-sm text-center">
+				<th class="border-0">Count</th>
+				<th class="border-0">ID</th>
+				<th class="border-0">First Name</th>
+				<th class="border-0">Last Name</th>
+				<th class="border-0">Skill Level</th>
+				<th class="border-0">Active Status</th>
+				<th class="border-0">&nbsp;</th>
+				<th class="border-0">&nbsp;</th>
+			</tr>
+		</thead>
+
+		<tbody class="draggable">
+			@php ($arr = rand(0, 10) > 0 ? range(1, rand(1, 50)) : [] )
+			@forelse ($arr as $v)
+			<tr class="card-body my-2 py-2 shadow-sm text-center">
+				<td class="border-0 align-middle">{{ str_pad($v, 2, '0', STR_PAD_LEFT) }}</td>
+				<td class="border-0 align-middle">{{ rand(100000, 999999) }}</td>
+				<td class="border-0 align-middle">{{ fake()->firstName() }}</td>
+				<td class="border-0 align-middle">{{ fake()->lastName() }}</td>
+				<td class="border-0 align-middle">{{ array('Home Cook', 'Beginner', 'Amateur', 'Experienced')[rand(0, 3)] }}</td>
+				
+				<td class="border-0 align-middle">
+					<div class="form-check form-switch d-flex px-0">
+						<input class="form-check-input mx-auto bg-orange border-orange" type="checkbox" role="switch" id="flexSwitchCheckDefault" {{ rand(0, 5) > 1 ? 'checked' : '' }}>
+					</div>
+				</td>
+				
+				<td class="border-0 align-middle">
+					<a href="javascript:console.log('Under development...');" class="btn btn-sm bg-light-orange text-orange px-3">Edit</a>
+				</td>
+
+				<td class="border-0 align-middle handle">
+					<i class="fas fa-ellipsis-vertical cursor-grab"></i>
+				</td>
+			</tr>
+			@empty
+			<tr class="card-body my-2 py-2 shadow-sm">
+				<td class="text-center border-0" colspan="8">Nothing to show~</td>
+			</tr>
+			@endforelse
+		</tbody>
+	</table>
+</div>
+@endsection
+
+@section('script')
+<script type="text/javascript">
+	$(document).ready(() => {
+		/**
+		 * Uses the jQuery UI sortable (https://api.jqueryui.com/sortable/)
+		 */
+		$(".draggable").sortable({
+			handle: ".handle"
+		});
+	});
+</script>
+@endsection
