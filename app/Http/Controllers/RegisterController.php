fec6647e2958177980868a1eb22f455b3fb96981
reydm-7166 <reymond.domingo.716@gmail.com>
2022-12-19 19:42:17 +0800

    added redirect facade

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Redirect;
 
 
 class RegisterController extends Controller
